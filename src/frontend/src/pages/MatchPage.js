db2388b37afca463410a7ac703e515dd88cfeb83
Anushang <anushangsrivastav@gmail.com>
2021-06-23 20:31:34 +0530

    Fixed react issues

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -5,11 +5,26 @@ import { useParams } from 'react-router-dom'
 
 export const MatchPage = () => {
 
-  
+  const [matches, setMatches] = useState([]);
+  const teamName="Delhi Capitals"
+  useEffect(  
+    () => {
+      const fetchMatches = async () => {
+        const respone = await fetch(`http://localhost:8080/team/${teamName}/matches?year=2019`);
+        const data = await respone.json();
+        setMatches(data);
+      };
+      fetchMatches();
+    }, []
+  )
 
   return (
     <div className="MatchPage">
       <h1>Match Page</h1>
+      {
+         matches.map(match => <MatchDetailCard teamName ={teamName} match={match}/>)
+
+      }
     </div>
   );
 }
