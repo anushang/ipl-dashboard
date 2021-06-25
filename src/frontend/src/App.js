3c3a335e965d5880871460549d449f624c32f375
Anushang <anushangsrivastav@gmail.com>
2021-06-25 23:34:16 +0530

    Styled teamPage

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 import { TeamPage } from './pages/TeamPage';
 import {MatchPage} from './pages/MatchPage';
 function App() {
--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { MatchDetailCard } from '../components/MatchDetailCard'
-import { MatchSmallCard } from '../components/MatchSmallCard'
-import { useParams } from 'react-router-dom'
+
 
 export const MatchPage = () => {
 
